71102df9abb265bc0c2bb02f022a3e4444697142
Dan Lefrancois <dan@14oranges.com>
2021-11-01 17:01:21 -0700

    Fixed an issue with the persistantReduce set up, it was not working as expected as only one persistant reducer should created - not two with the same key.

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import FSStorage from 'redux-persist-fs-storage';
@@ -8,18 +9,20 @@ import systemInfoReducer from './SystemInfoSlice';
 const persistConfig = {
   key: 'root',
   keyPrefix: '', // the redux-persist default is `persist:` which doesn't work with some file systems
+  blacklist: ['session'], // session is not persisted, new token is retrieved via credential storage if needed
   storage: FSStorage(),
 };
 
-const persistedBrandInfoReducer = persistReducer(persistConfig, brandInfoReducer);
-const persistedSystemInfoReducer = persistReducer(persistConfig, systemInfoReducer);
+const rootReducer = combineReducers({
+  session: sessionReducer,
+  brandInfo: brandInfoReducer,
+  systemInfo: systemInfoReducer,
+});
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    session: sessionReducer, // session is not persisted, new token is retrieved via credential storage if needed
-    brandInfo: persistedBrandInfoReducer,
-    systemInfo: persistedSystemInfoReducer,
-  },
+  reducer: persistedRootReducer,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: false,
