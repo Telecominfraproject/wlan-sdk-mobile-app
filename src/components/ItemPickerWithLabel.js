b74129fc2cecb2bbbe57afa43ba000e362fc5272
Dan Lefrancois <dan@14oranges.com>
2022-03-05 19:33:38 -0800

    Added in support for showing an activity indicator when changing a dropdown setting.

--- src/components/ItemPickerWithLabel.js
+++ src/components/ItemPickerWithLabel.js
@@ -1,18 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { ActivityIndicator, Alert, Pressable, StyleSheet, Text, View } from 'react-native';
-import { grayColor, heightCellDefault, paddingHorizontalDefault, primaryColor, whiteColor } from '../AppStyle';
+import {
+  grayColor,
+  heightCellDefault,
+  paddingHorizontalDefault,
+  primaryColor,
+  whiteColor,
+  blackColor,
+} from '../AppStyle';
 import ButtonStyled from '../components/ButtonStyled';
 import DropDownPicker from 'react-native-dropdown-picker';
 import isEqual from 'lodash.isequal';
 
 const ItemPickerWithLabel = props => {
   const { label = '', disabled = false, disabledReason } = props;
+  const [changeLoading, setChangeLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const [items, setItems] = useState(props.items ?? []);
   const placeholder = props.placeholder ?? 'Select an item';
+  const isMounted = useRef(false);
+
+  useEffect(() => {
+    isMounted.current = true;
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
   const onChangeValue = async value => {
     try {
+      setChangeLoading(true);
+
       if (props.onChangeValue) {
         let updateValue = props.changeKey ? { [props.changeKey]: value } : value;
         // Use a promise to ensure we can call Async functions. By using Promise.resolve
@@ -21,6 +40,13 @@ const ItemPickerWithLabel = props => {
       }
     } catch (error) {
       // Do nothing
+    } finally {
+      // The onEdit call may result in a re-render of the parent and having this component be
+      // unmounted. In this case we have no need to update the information and it will result
+      // in a warning if we do. So detect this condition
+      if (isMounted.current) {
+        setChangeLoading(false);
+      }
     }
   };
 
@@ -28,6 +54,22 @@ const ItemPickerWithLabel = props => {
     if (disabled && disabledReason) {
       Alert.alert(label, disabledReason, undefined, { cancelable: true });
     }
+  };
+
+  const getCurrentValue = () => {
+    console.log(items);
+    if (items && props.value) {
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i];
+        let itemValue = item.value;
+
+        if (itemValue === props.value) {
+          return item.label;
+        }
+      }
+    }
+
+    return '';
   };
 
   const componentStyles = StyleSheet.create({
@@ -50,12 +92,28 @@ const ItemPickerWithLabel = props => {
       flexWrap: 'nowrap',
       justifyContent: 'space-evenly',
     },
+    activityIndicatorContainer: {
+      width: '100%',
+      // Layout
+      flexDirection: 'row',
+      flexWrap: 'nowrap',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
     activityIndicator: {
-      height: 28,
+      width: 16,
+      height: 16,
     },
     textLabel: {
       fontSize: 11,
       color: primaryColor,
+    },
+    textValue: {
+      height: 28,
+      lineHeight: 28,
+      fontSize: 14,
+      textAlignVertical: 'center',
+      color: blackColor,
     },
     picker: {
       height: 28,
@@ -83,13 +141,18 @@ const ItemPickerWithLabel = props => {
         <Text style={componentStyles.textLabel} numberOfLines={1}>
           {label}
         </Text>
-        {props.loading ? (
-          <ActivityIndicator
-            style={componentStyles.activityIndicator}
-            size="large"
-            color={primaryColor}
-            animating={props.loading}
-          />
+        {changeLoading || props.loading ? (
+          <View style={componentStyles.activityIndicatorContainer}>
+            <Text style={componentStyles.textValue} numberOfLines={1}>
+              {getCurrentValue()}
+            </Text>
+            <ActivityIndicator
+              style={componentStyles.activityIndicator}
+              size="large"
+              color={primaryColor}
+              animating={props.loading}
+            />
+          </View>
         ) : (
           <DropDownPicker
             listMode={'SCROLLVIEW'}
